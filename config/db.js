f998bf2a15d1afde2d1bef5e228e62663be1e01c
Evan Szymkowicz <evan.szymkowicz@evanwolf.tech>
2020-09-16 13:48:10 -0400

    css cleanup

--- config/db.js
+++ config/db.js
@@ -8,7 +8,7 @@ const connectDB = async () => {
 			useNewUrlParser: true,
 			useCreateIndex: true,
 			useFindandModify: false,
-			dbName: 'the-boss'
+			dbName: 'the_boss'
 		});
 		console.log('MongoDB is connected.');
 	} catch (error) {
