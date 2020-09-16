e701a164f3e51bc08c3eea18832e2f5d1ebe889a
Evan Szymkowicz <evan.szymkowicz@evanwolf.tech>
2020-09-15 21:46:48 -0400

    css cleanup

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,15 +4,12 @@ export default function Home() {
     return (
         <>
             <section className="home__landing">
-                <h1 className="home__title">Rick Ross</h1>
-                <video 
-                className="home__video"
-                width="70%" height="auto" controls muted loop autoPlay playsInline >
-                    <source src="/imgs/rickross-musicvideo.gif" type="gif" />
+                <video className="home__video" width="70%" height="auto" >
+                    <source src="/imgs/homepage-video.gif" type="" />
                     Your browser doesn't support the video tag.
                 </video>
                 <div className="home__video-text">
-                    <p className="subtitle">Where to see The Boss:</p>
+                    <p className="subtitle">The Boss</p>
                     <div className="arrow">
                         <span></span>
                         <span></span>
@@ -23,34 +20,47 @@ export default function Home() {
                 
             <section className="tour">
                 <div className="tour__item">
-                    <img src="/imgs/live-southafrica.jpeg" alt="tour washington, d.c."/>
+                    <img src="/imgs/god-forgives-bg.jpg" alt="tour washington, d.c."/>
                     <div className="tour__text">
                         <p>Capital One Arena, Washington, D.C.</p>
                         <p>April 9</p>
                     </div>
                 </div>
                 <div className="tour__item">
-                    <img src="/imgs/live-the-boss.jpeg" alt="tour miami"/>
+                    <img src="/imgs/miami-heat.jpg" alt="tour miami"/>
                     <div className="tour__text">
-                        <p>Hard Rock Stadium, Miami</p>
+                        <p>Hard Rock Stadium</p><p>Miami</p>
                         <p>Feburary 12</p>
                     </div>
                 </div>
                 <div className="tour__item">
-                    <img src="/imgs/port-of-miami.jpeg" alt="tour greensboro"/>
+                    <img src="/imgs/teflon-don-bg.jpg" alt="tour greensboro"/>
                     <div className="tour__text">
                         <p>Greensboro Coliseum Complex, Greensboro</p>
                         <p>April 2</p>
                     </div>
                 </div>
                 <div className="tour__item">
-                    <img src="/imgs/trilla.jpeg" alt="tour canalside"/>
+                    <img src="/imgs/mastermind.png" alt="tour canalside"/>
                     <div className="tour__text">
                         <p>Canalside, Buffalo</p>
                         <p>August 07</p>
                     </div>
                 </div>
-                
+                <div className="tour__item">
+                    <img src="/imgs/wallpaper-5.jpg" alt="tour dallas"/>
+                    <div className="tour__text">
+                        <p>Music Hall, Dallas</p>
+                        <p>TBA</p>
+                    </div>
+                </div>
+                <div className="tour__item">
+                    <img src="/imgs/wallpaper-6.jpg" alt="tour LA"/>
+                    <div className="tour__text">
+                        <p>The Forum</p>
+                        <p>August 07</p>
+                    </div>
+                </div>
             </section>
         </>
     )
--- client/src/components/Story.js
+++ client/src/components/Story.js
@@ -4,42 +4,38 @@ export default function Story() {
     return (
         <>
             <section className="story-intro">
-                <img src="/imgs/2020-21-calendar.jpeg" alt="rick ross introduction" />
+                <img src="/imgs/2021-calendar.jpeg" alt="rick ross introduction" />
                 <h2 className="story-intro-text">The Boss</h2>
             </section>
 
-            <section className="rickross-bg rickross-bg2018">
-                <span className="story-year story-year--right">2018</span>
-                <img src="/imgs/rather-you-than-me.jpeg" alt="rick-ross band in 2018" />
+            <section className="rickross-bg rickross-bg2006">
+                <span className="story-year story-year--right">2006</span>
                 <div className="rick-ross-text rick-ross-text--right">
-                    <h2>Current</h2>
+                    <h2>2006: Port of Miami</h2>
                     <p>
-                     He announced that his ninth studio album, Rather You Than Me, was slated for a March 2017 release. The album debuted and peaked number three on the Billboard 200.[49] On February 2, 2018, Rick Ross released "Florida Boy" with T-Pain and Kodak Black as the first single from the album, which was slated for a 2018 release. On June 22, 2018, Ross released "Green Gucci Suit" with Future as the second single from his tenth studio album Port of Miami 2, released on August 9, 2019. The album debuted at number two on the Billboard 200   
-                    </p>
-                    <p>
-                    On July 19, 2020, Ross teamed up with singer August Alsina for a track titled "Entanglements", a diss track towards Alsina's ex-lover Jada Pinkett Smith.[52] On August 7, 2020, Ross released the single "Pinned to the Cross", featuring Finn Matthews, following his Verzuz battle with 2 Chainz
+                    Rick Ross' debut album Port of Miami was released in August 2006 and debuted at the top spot on the U.S. Billboard 200 album chart, with sales at 187,000 units during the first week. Christian Hoard of Rolling Stone magazine predicted that it would be "the summer's biggest rap record".[14] The second single was "Push It", which samples "Scarface (Push It to the Limit)", the theme song from the gangster film Scarface.[15] The music video for "Push It" was modeled after the film.[16] During that time, Ross made guest performances on two singles from DJ Khaled's debut Listennn... the Album: "Born-N-Raised" and "Holla at Me". Port of Miami received Gold certification from the Recording Industry Association of America on November 8, 2006.
                     </p>
                 </div>
             </section>
 
             <section className="rickross-bg rickross-bg2013">
                 <span className="story-year story-year--left">2013</span>
-                <img className="rick-ross-img-right" src="/imgs/rick-ross-2013.jpg" alt="rick-ross band in 2013" />
+                
                 <div className="rick-ross-text rick-ross-text--left">
                     <h2>Prime Rick Ross</h2>
                     <p>
-                    On September 5, 2013, Ross premiered the first official single from Mastermind, "No Games" featuring Future produced by J.U.S.T.I.C.E. League.[38] The following day, it was released to Urban contemporary radio.[39] It was followed by the singles "The Devil Is A Lie" with Jay-Z and "War Ready" with Jeezy.
+                    On September 5, 2013, Ross premiered the first official single from Mastermind, "No Games" featuring Future produced by J.U.S.T.I.C.E. League. The following day, it was released to Urban contemporary radio.[39] It was followed by the singles "The Devil Is A Lie" with Jay-Z and "War Ready" with Jeezy.
                     </p>
                     <p>
-                     Less than a month later, Ross' fifth studio album, God Forgives, I Don't, was released on July 31, 2012 after a few delays.[27][28] Upon its release, the album charted at number eight on the UK Albums Chart and number two on the UK R&B Albums Chart, making it Ross's highest-charting album and first top 10 album in the United Kingdom. The album debuted at number one on the Billboard 200, with first-week sales of 218,000 copies.[29] Ross premiered four songs: "So Sophisticated" featuring Meek Mill, "Touch'N You" featuring Usher, "Hold Me Back", and "3 Kings" featuring Dr. Dre and Jay-Z. The album was certified gold in less than two months. To celebrate his accomplishment and promote his upcoming MMG Tour, Ross released a mixtape, The Black Bar Mitzvah in October. Rick Ross was nominated by The Source as its "Man of the Year"
+                     Less than a month later, Ross' fifth studio album, God Forgives, I Don't, was released on July 31, 2012 after a few delays. Upon its release, the album charted at number eight on the UK Albums Chart and number two on the UK R&B Albums Chart, making it Ross's highest-charting album and first top 10 album in the United Kingdom. The album debuted at number one on the Billboard 200, with first-week sales of 218,000 copies.[29] Ross premiered four songs: "So Sophisticated" featuring Meek Mill, "Touch'N You" featuring Usher, "Hold Me Back", and "3 Kings" featuring Dr. Dre and Jay-Z. The album was certified gold in less than two months. To celebrate his accomplishment and promote his upcoming MMG Tour, Ross released a mixtape, The Black Bar Mitzvah in October. Rick Ross was nominated by The Source as its "Man of the Year"
                     </p>
                 </div>
             </section>
 
             <section className="rickross-bg rickross-bg2014">
                 <span className="story-year story-year--right">2014</span>
-                <img src="/imgs/" alt="rick ross in 2014" />
-                <div className="rickross-text rickross-text--right">
+                
+                <div className="rick-ross-text rick-ross-text--right">
                     <h2>2014: Mastermind and Hood Billionaire</h2>
                      <p>
                     Rick Ross released his sixth studio album, Mastermind, in March 2014, the album received generally positive reviews from critics and debuted at number one on the Billboard 200 chart with first week sales of 179,000 copies.[40] As of April 8, 2014, the album has sold over 290,000 copies in the United States.[41] In June 2014, it was announced that Ross would make a guest appearance on the docu-series Sisterhood of Hip Hop.
@@ -48,27 +44,19 @@ export default function Story() {
                 </div>
             </section>
 
-            <section className="rickross-bg rickross-bg2010">
-                <span className="story-year story-year--left">2010</span>
-                <img className="rickross-img-right" src="/imgs/rick-ross-2005.jpg" alt="rick ross " />
-                <div className="rickross-text rick-ross-text--left">
-                    <h2>2010-2013: Ascent</h2>
+             <section className="rickross-bg rickross-bg2018">
+                <span className="story-year story-year--left">2018</span>
+                <div className="rick-ross-text rick-ross-text--left">
+                    <h2>Current</h2>
                     <p>
-                    In May 2010 he released the critically acclaimed Albert Anastasia EP. In addition to that, he released his fourth studio album entitled Teflon Don, on July 20, 2010. The lead single was "Super High" featuring Ne-Yo. Actress Stacey Dash made an appearance in the music video. A second official single was released titled "Live Fast, Die Young" which features and is produced by Kanye West. The album debuted at number two on the Billboard 200 selling 176,000 copies in its first week.
+                     He announced that his ninth studio album, Rather You Than Me, was slated for a March 2017 release. The album debuted and peaked number three on the Billboard 200. On February 2, 2018, Rick Ross released "Florida Boy" with T-Pain and Kodak Black as the first single from the album, which was slated for a 2018 release. On June 22, 2018, Ross released "Green Gucci Suit" with Future as the second single from his tenth studio album Port of Miami 2, released on August 9, 2019. The album debuted at number two on the Billboard 200   
+                    </p>
+                    <p>
+                    On July 19, 2020, Ross teamed up with singer August Alsina for a track titled "Entanglements", a diss track towards Alsina's ex-lover Jada Pinkett Smith. On August 7, 2020, Ross released the single "Pinned to the Cross", featuring Finn Matthews, following his Verzuz battle with 2 Chainz
                     </p>
                 </div>
             </section>
-
-            <section className="rickross-bg rickross-bg2006">
-                <span className="story-year story-year--right">The Beginning</span>
-                <img src="/imgs/rick-ross-2002.jpg" alt="rick ross in 2006: port of miami" />
-                <div className="rick-ross-text rick-ross-text--right">
-                    <h2>2006: Port of Miami</h2>
-                    <p>
-                    Rick Ross' debut album Port of Miami was released in August 2006 and debuted at the top spot on the U.S. Billboard 200 album chart, with sales at 187,000 units during the first week.[4][5] Christian Hoard of Rolling Stone magazine predicted that it would be "the summer's biggest rap record".[14] The second single was "Push It", which samples "Scarface (Push It to the Limit)", the theme song from the gangster film Scarface.[15] The music video for "Push It" was modeled after the film.[16] During that time, Ross made guest performances on two singles from DJ Khaled's debut Listennn... the Album: "Born-N-Raised" and "Holla at Me". Port of Miami received Gold certification from the Recording Industry Association of America on November 8, 2006.
-                    </p>
-                </div>
-            </section>
+        
         </>
     )
 }
